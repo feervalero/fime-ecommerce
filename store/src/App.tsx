e3d61e8a9b0744bf1e1044ace0622b382284760b
Fernando Valero <feervalero@gmail.com>
2021-03-26 00:00:40 -0600

    test 12

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/config" component={Config}/>
-        <Route path="/" component={Home}/>
+        <Route path="/store/config" component={Config}/>
+        <Route path="/store" component={Home}/>
       </Switch>
     </Router>
   );
